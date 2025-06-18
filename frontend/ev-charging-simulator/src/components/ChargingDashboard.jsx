6e2b27bbb51cbba7cdb97e0d2839c028f7dac785
VipeR-69 <computershubham7@gmail.com>
2025-06-18 14:10:11 +0530

    Battery UI changes

--- frontend/ev-charging-simulator/src/components/ChargingDashboard.jsx
+++ frontend/ev-charging-simulator/src/components/ChargingDashboard.jsx
@@ -35,7 +35,7 @@ export default function ChargingDashboard() {
         style={{ backgroundImage: `url(${batteryBg})` }}
       >
         <div
-          className="absolute bottom-[13.6%] left-[39.5%] w-[22%] bg-green-500 transition-all duration-500 z-10 rounded-xl"
+          className="absolute bottom-[13.5%] left-[39.6%] w-[21.8%] bg-green-500 transition-all duration-500 z-10 rounded-b-xl"
           style={{ height: `${soc * 0.62}%`, opacity: 0.7 }}
         ></div>
         <div className="absolute inset-0 flex flex-col justify-center items-center text-black text-lg font-semibo drop-shadow-md z-20">
